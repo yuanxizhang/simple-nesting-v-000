a1939334b115f38d027b244a48550a43e508a122
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-07 02:59:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
   programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
   return programmer_hash
 end
 
